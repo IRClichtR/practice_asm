19dcdd6e868e2267fdad070107bbd34752dd94a5
Floriane TUERNAL SABOTINOV <ftuertinov@proton.me>
2025-03-25 20:39:08 +0100

    write

--- mandatory/ft_write.s
+++ mandatory/ft_write.s
@@ -1,9 +1,17 @@
-section .data
-    err_msg "Error opening file", 0
-    err_msg_len equ $ - err_msg
-
-
 section .text
     global ft_write
+    extern __errno_location
 
 ft_write:
+    mov rax, 1
+    syscall
+    test rax, rax
+    js .error
+    ret
+.error:
+    neg rax
+    mov rdi, rax
+    call __errno_location
+    mov [rax], rdi
+    mov rax, -1\
+    ret
