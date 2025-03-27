d4bf808e5ff29c5a91facfe71c157a7fba9c8088
IRClichtR <ftuertinov@proton.me>
2025-03-27 17:11:29 +0100

    continue on ft_strdup

--- mandatory/ft_strdup.s
+++ mandatory/ft_strdup.s
@@ -2,8 +2,16 @@ section .text
     global ft_strdup
     extern ft_strlen
     extern malloc
+    extern __errno_location
 
 ft_strdup:
     mov rdi, rax
     call ft_strlen
+    mov rax, rsi
+    ret
+.error
+    neg rax
+    call __errno_location WRT ..plt
+    mov [rax], rdi
+    mov rax, -1
     ret
